eb4aebe9a762ed4dc3b6301d9377cc5e8794307b
baabar02 <baabar02githu.com>
2025-05-27 12:30:20 +0800

    json

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+app.use(express.json());
+const fs = require("fs");
 
-app.get("/g",(req,res) => {
-res.send("hello world!")
-})
+app.get("/adduser", (req, res) => {
+  const data = fs.readFileSync("./users.json", "utf-8");
+  res.send(data);
+});
 
-const array = [1, 2, 3, 4];
+app.post("/adduser", async (req, res) => {
+  await fs.writeFileSync("./users.json", JSON.stringify(req.body), "utf-8");
+  res.send("Successfully ");
+});
+
+app.put("/adduser", async (req, res) => {
+  const data = await fs.readFileSync("./users.json", "utf-8");
+  const result = JSON.parse(data);
+  dataJSON = { ...result, userName: "Tengis" };
+  await fs.writeFileSync("./users.json", JSON.stringify(dataJSON), "utf-8");
+  res.send("UserName updated");
+});
+
+app.put("/add", async (req, res) => {
+  const data = await fs.readFileSync("./users.json", "utf-8");
+  const result = JSON.parse(data);
+  dataJSON = { ...result, phone: "99999911" };
+  await fs.writeFileSync("./users.json", JSON.stringify(dataJSON), "utf-8");
+  res.send("phone updated");
+});
+
+app.delete("/add", async (req, res) => {
+  const data = await fs.readFileSync("./users.json", "utf-8");
+  const result = JSON.parse(data);
+  const { age, ...rest } = result;
+  const dataJSON = rest;
+  await fs.writeFileSync("./users.json", JSON.stringify(dataJSON), "utf-8");
+  res.send("age deleted");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port${port}`);
+});
+
+// const array = [];
+// const newArray = [{ name: "baabar", password: "password" }];
+
+// app.post("/test", (req, res) => {
+//   array.push(req.body);
+
+//   res.send("amjilttai");
+// });
+
+// app.get("/test", (req, res) => {
+//   res.status(200).send(array);
+// });
+
+// const array = [1, 2, 3, 4];
 
 // app.get("/movie/popularlanguage=en-US&page=1", (req, res) => {
 //   res.status(200).send([
@@ -18,51 +68,31 @@ const array = [1, 2, 3, 4];
 //   ]);
 // });
 
-app.get("/test", (req, res) => {
-  res.status(200).send(array);
-});
-
-app.get("/test", (req,res) =>{
-  const value
-})
-
-app.post("/test", (req, res) => {
-  array.push("5");
-  res.status(200).send("successfully created");
-});
-
-app.delete("/test", (req, res) => {
-  array.shift();
-  res.status(200).send("successfully deleted");
-});
-
-app.post("/init", (req, res) => {
-  array.pop();
-  res.status(200).send("successfully removed last element")
-});
-
-app.post("/reset", (req, res) => {
-  array[0] = 1;
-  array[1] = 2;
-  array[2] = 3;
-  array[3] = 4;
-  res.send("successully reset");
-});
-
-// app.get("/movie/popularlanguage=en-US&page=1", (id, res) => {
-//   res.status(300).send([{ id: "234" }]);
-// });
-// app.get("/movie/upcominglanguage=en-US&page=1", (req, res) => {
-//   res.send({ title: "bond" });
-// });
-// app.get("/movie/top_ratedlanguage=en-US&page=1", (req, res) => {
-//   res.send({ id: "Error" });
-// });
-// app.get("/movie/now_playinglanguage=en-US&page=1", (req, res) => {
-//   res.send({ id: "Error" });
+// app.get("/test", (req, res) => {
+//   res.status(200).send(array);
 // });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port${port}`);
-});
+// app.get("/test", (req, res) => {});
 
+// app.post("/test", (req, res) => {
+//   array.push("5");
+//   res.status(200).send("successfully created");
+// });
+
+// app.delete("/test", (req, res) => {
+//   array.shift();
+//   res.status(200).send("successfully deleted");
+// });
+
+// app.post("/init", (req, res) => {
+//   array.pop();
+//   res.status(200).send("successfully removed last element");
+// });
+
+// app.post("/reset", (req, res) => {
+//   array[0] = 1;
+//   array[1] = 2;
+//   array[2] = 3;
+//   array[3] = 4;
+//   res.send("successully reset");
+// });
