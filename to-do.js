6a241ef6c80d5af190127a24ff729fc4e9e45e7c
baabar02 <baabarmx@gmail.com>
2025-05-28 22:32:45 +0800

    ads

--- to-do.js
+++ to-do.js
@@ -4,26 +4,27 @@ const app = express();
 const port = 8000;
 app.use(express.json());
 const fs = require("fs");
+const todoArray = []
 
-app.get("/to-do", async (req, res) => {
+app.get("/", async (req, res) => {
   try {
     const data = await fs.readFileSync("./todo.json", "utf-8");
-
-    res.send(data);
-  } catch {
-    error;
-  }
-
-  res.status(200).send(data);
+    const dataJSON = JSON.parse(data)
+    res.status(200).send(dataJSON);
+  } catch (error) {
+    res.status(400).send(`file not found ${error.message}`);
+  } 
 });
 
-app.post("/to-do", async (req, res) => {
-  await fs.writeFileSync("./todo.json", JSON.stringify(req.body), "utf-8");
+app.post("/", async (req, res) => {
+
+  todoArray.push(req.body)
+  await fs.writeFileSync("./todo.json", JSON.stringify(todoArray), "utf-8");
 
   res.send("Successfully");
 });
 
-app.put("/to-do", async (req, res) => {
+app.put("/", async (req, res) => {
   try {
     const data = await fs.readFileSync("./todo.json", "utf-8");
     const result = JSON.parse(data);
@@ -33,22 +34,67 @@ app.put("/to-do", async (req, res) => {
     await fs.writeFileSync("./todo.json", JSON.stringify(dataJSon), "utf-8");
 
     res.send("successully");
-  } catch {
-    error;
+  } catch (error) {
   }
-  res.send("aldaa");
+  res.send(`aldaa${error.message}`);
 });
 
-// app.put("/to-do", async (req, res) => {
-//   const data = await fs.readFileSync("./todo.json", "utf-8");
-//   const result = JSON.parse(data);
-//   const newTask = "hoolloh";
-//   if (!newTask) {
-//     result.task = newTask;
-//   }
-//   await fs.writeFileSync("./todo.json", JSON.stringify(result), "utf-8");
-//   res.send("new task added");
-// });
+
+
+
+const data = fs.readFileSync("./todo.json", "utf-8")
+const dataJSON = JSON.parse(data)
+ 
+app.post("/", async(req, res)=>{
+  try{
+    const result =[...dataJSON,{task: req.body.task, id:req.body.id, isDone:req.body.isDone, userName:req.body.userName},];
+    await fs.writeFileSync("./todo.json", JSON.stringify(result),"utf-8");
+    res.send("successfully updated");
+  } catch (error) {
+    res.send(error.message);
+  }
+})
+
+app.put("/", async(req, res)=>{
+  const doneData = dataJSON.map((el)=>{
+    if(el.id === req.body.id) {
+      return {...el, ...req.body};
+    } else {
+      return el;
+    }
+  });
+  await fs.writeFileSync("./todo.json", JSON.stringify(doneData), "utf-8");
+  res.send("successfully Done")
+});
+ 
+app.delete("/:id", async(req, res)=>{
+
+  const {id} =req.body
+if(!id) {
+  return res.status(400).json({error:"id is required"})
+}
+try {
+const data = await fs.readFile("./todo.json", "utf-8");
+    const dataJSON = JSON.parse(data);
+}  catch(error) {
+  if(error.code === "ENOENT") {
+    dataJSON =[]
+  } else {
+    throw error;
+  }
+}
+
+  const deletedData = dataJSON.filter((el)=> el.id !== id);
+
+    if(deletedData.lenght === dataJSON.lenght) {
+      return res.send(404).json({error:"to-do not found"})
+    }
+  ;
+  await fs.writeFileSync("./todo.json", JSON.stringify(deletedData),"utf-8");
+  res.send.json("Deleted")
+})
+ 
+ 
 
 app.listen(port, () => {
   console.log(`example to-do app listening on port ${port}`);
