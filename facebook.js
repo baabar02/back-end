047288ed456a1edb8cd6233ae6bdeaec2d544d09
baabar02 <baabarmx@gmail.com>
2025-05-31 20:34:52 +0800

    ads

--- facebook.js
+++ facebook.js
@@ -1,3 +1,4 @@
+
 const { error } = require("console");
 const express = require("express");
 const app = express();
@@ -20,51 +21,73 @@ app.post("/", async (req, res) => {
   const data = fs.readFileSync("./fb.json", "utf-8");
   const dataJSON = JSON.parse(data);
 
-  const userExisting = dataJSON.find(
+  const userExists = dataJSON.some(
     (el) =>
-      el.email === req.body.email && el.mobileNumber === req.body.mobileNumber
+      el.email.toLowerCase() === req.body.email.toLowerCase() && el.mobileNumber === req.body.mobileNumber
   );
-  const userEmail = dataJSON.find((el) => el.email === req.body.email);
-  if (userExisting) {
-    return res.send("Hereglegch burtgeltei baina");
-  } else if (userEmail) {
-    return res.send("Hereglegch burtgeltei baina");
-  } else {
-    dataJSON.push(req.body);
-    await fs.writeFileSync("./fb.json", JSON.stringify(dataJSON), "utf-8");
-    return res.send("successfully");
+  if(userExists) {
+    return res.status(400).send("Hereglegch burtgeltei baina")
+  } 
+dataJSON.push(req.body)
+fs.writeFileSync("./fb.json", JSON.stringify(dataJSON), "utf-8")
+res.status(200).send("successfully")
+})
+
+
+app.post("/signup", async(req,res)=>{
+  const data = fs.readFileSync("./fb.json", "utf-8");
+  const dataJSON = JSON.parse(data);
+  const {firstName, lastName, birthday, email, mobileNumber} = req.body;
+  const trimmedFirstName = firstName.trim();
+  const trimmedLastname = lastName.trim();
+  
+
+  if(!firstName || !lastName || !birthday || !email || !mobileNumber) {
+
+    return res.status(400).send("All fields (firstName, lastName, birthday, email, mobileNumber) are required")
   }
-});
 
-// app.put("/", async (req, res) => {
-//   const data = fs.readFileSync("./fb.json", "utf-8");
-//   const dataJSON = JSON.parse(data);
-//   const userEmail = dataJSON.find((el) => {
-//     if (el.email !== req.body.email) {
-//       return { ...el, email: req.body.email };
-//     } else {
-//       return el;
-//     }
-//   });
+  const potentialDuplicate = dataJSON.find((el)=>el.firstName === trimmedFirstName && el.lastName === trimmedLastname && el.birthday === birthday ); 
+  if(potentialDuplicate) {
+    console.log("Warning: User with same name and birthday exists");
+    
+  }
+//   const birthdayRegex = /^\d{4}-\d{2}-\d{2}$/;
+// if(birthdayRegex.test(birthday)){
+//   return res.status(400).send("Birthday must be in YYYY-MM-DD format")
+// }
 
-//   await fs.writeFileSync("./fb.json", JSON.stringify(userEmail), "utf-8");
-//   res.send("successfully");
-// });
+// const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+// if(emailRegex.test(email)) {
+//   return res.status(400).send("Invalid email format")
+// }
 
-// app.post("/", async (req, res) => {
-//   const result = [
-//     ...dataJSON,
-//     {
-//       password: req.body.password,
-//       id: req.body.id,
-//       mobileNumber: req.body.mobileNumber,
-//       email: req.body.email,
-//     },
-//   ];
-//   Arr.push(result);
-//   await fs.writeFileSync("./todo.json", JSON.stringify(result), "utf-8");
-//   res.send("successfully");
-// });
+const newUser = {
+  id:dataJSON.lenght + 1,
+  firstName: trimmedFirstName,
+  lastName: trimmedLastname,
+  birthday,
+  email,
+  mobileNumber
+}
+
+dataJSON.push(newUser)
+fs.writeFileSync("./fb.json", JSON.stringify(dataJSON), "utf-8")
+res.status(200).send("User registered successfully")
+})
+
+app.get("/", (req, res)=>{
+  try{
+ const data = fs.readFileSync("./fb.json", "utf-8");
+  const dataJSON = JSON.parse(data);
+  res.status(200).send(dataJSON)
+  } catch (error) {
+    res.status(500).send(`error reading dat ${error.message}`)
+  }
+ 
+
+})
+
 
 app.listen(port, () => {
   console.log(`example to-do app listening on port ${port}`);
